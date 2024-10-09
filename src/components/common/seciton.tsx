85a53eb9b77bc03e6768f5d12b1d639a59cb593b
kirthi1594 <kirthikajayaraman1594@gmail.com>
2024-10-09 15:02:21 +0530

    section responsive updated

--- src/components/common/seciton.tsx
+++ src/components/common/seciton.tsx
@@ -14,11 +14,16 @@ const Section = ({
   buttonLink,
 }: any) => {
   return (
-    <div className="max-lg:max-w-6xl m-auto xl:max-w-7xl">
-      <div
+    <div className="lg:max-w-xl m-auto xl:max-w-6xl px-4 sm:px-6">
+      {/* <div
         className={`flex  ${
           reverse ? "lg:flex-row-reverse " : ""
         } items-center lg:items-center space-y-6 lg:space-y-0 lg:space-x-10 max-md:gap-10`}
+      > */}
+        <div
+        className={`flex flex-col-reverse lg:flex-row ${
+          reverse ? "lg:flex-row-reverse" : ""
+        } items-center space-y-6 lg:space-y-0 lg:space-x-10 max-md:gap-10`}
       >
         {/* Image Section */}
         <div
@@ -30,7 +35,15 @@ const Section = ({
           data-aos-mirror="true"
           data-aos-once="false"
         >
-          <div className="relative w-full md:h-[300px] !h-96 max-md:w-auto lg:h-[400px] rounded-lg overflow-hidden shadow-lg max-md:mx-5 bg-slate-600 ">
+          {/* <div className="relative w-full md:h-[300px] !h-96 max-md:w-auto lg:h-[400px] rounded-lg overflow-hidden shadow-lg max-md:mx-5 bg-slate-600 ">
+            <Image
+              src={image}
+              alt={title}
+              layout="fill"
+              objectFit="cover"
+              className="rounded-lg"
+            /> */}
+                      <div className="relative w-full h-[250px] sm:h-[300px] lg:h-[400px] rounded-lg overflow-hidden shadow-lg bg-slate-600">
             <Image
               src={image}
               alt={title}
@@ -42,10 +55,10 @@ const Section = ({
         </div>
 
         {/* Content Section */}
-        <div className="w-full lg:w-1/2 px-6 lg:px-0 ">
+        <div className="w-full lg:w-1/2 px-6 lg:px-0 sm:px-6">
           <TitleWithNumber title={title} number={titleNumber} />
           <div className="mb-10">
-            <p className="text-lg text-gray-950 mt-4 leading-relaxed font-rodin">
+            <p className="text-base sm:text-lg text-gray-950 mt-4 leading-relaxed font-rodin">
               {content}
             </p>
           </div>
